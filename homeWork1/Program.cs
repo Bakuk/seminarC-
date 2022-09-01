fbe4c6e6acf0087d22e56695ca5ef1e0925393ef
Bakuk <bakuk1996@mail.ru>
2022-09-01 12:00:59 +0700

    homeWork1

--- homeWork1/Program.cs
+++ homeWork1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+//int max;
+if(a>b){
+    //max = a;
+    Console.WriteLine("max= " + a);
+} else{
+    Console.WriteLine("max= " +  b);
+}
