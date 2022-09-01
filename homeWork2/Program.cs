37bdd53340017f597d5aaae30ee41c225fc69928
Bakuk <bakuk1996@mail.ru>
2022-09-01 14:22:17 +0700

    homeWork2

---
+++ homeWork2/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. */
+
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int c = Int32.Parse(Console.ReadLine());
+
+if(a>b && a>c){
+    Console.WriteLine("max= " + a);
+} else if(b>a && b>c ){
+    Console.WriteLine("max= " + b);
+} else if (c>a && c>b){
+    Console.WriteLine("max= " + c);
+}
