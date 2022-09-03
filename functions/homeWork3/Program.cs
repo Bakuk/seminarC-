c5bda8622475be25fa556c1e23e8f50bcbc7cc61
Bakuk <bakuk1996@mail.ru>
2022-09-03 23:16:05 +0700

    homeWork3

---
+++ functions/homeWork3/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+int a = Int32.Parse(Console.ReadLine());
+
+void сheckingTheDayOfTheWeek (int week){
+    switch (week){
+        case 1:
+            Console.WriteLine("нет");
+            break;
+        case 2:
+            Console.WriteLine("нет");
+            break;
+        case 3:
+            Console.WriteLine("нет");
+            break;
+        case 4:
+            Console.WriteLine("нет");
+            break;
+        case 5:
+            Console.WriteLine("нет");
+            break;
+        case 6:
+            Console.WriteLine("да");
+            break;
+        case 7:
+            Console.WriteLine("да");
+            break;
+        default:
+            Console.WriteLine("нет такого дня");
+            break;
+    }
+} 
+
+сheckingTheDayOfTheWeek(a);
+
