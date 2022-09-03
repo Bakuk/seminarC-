1d9278084d8f25ca71cc8922b1545800a32474f7
Bakuk <bakuk1996@mail.ru>
2022-09-03 22:58:39 +0700

    homeWork2

--- functions/homeWork2/Program.cs
+++ functions/homeWork2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. */
+
+int a = 12;
+
+void returnThirdNum(int n){
+    string convertArray = n.ToString();
+    int[] arrInt = new int[convertArray.Length];
+
+    for(int i = 0; i < convertArray.Length; i++){ 
+        arrInt[i] = int.Parse(convertArray[i]+ "");
+        //Console.WriteLine(arrInt[i]);
+    }
+    if(arrInt.Length<3){
+        Console.WriteLine("Нет третьего числа");    
+    } else {
+        Console.WriteLine(arrInt[2]);
+    }
+     
+}
+
+returnThirdNum(a);
+//Console.WriteLine(returnThirdNum(a));
+
+
