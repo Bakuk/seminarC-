459d5535d2ec4ac060af6353b8c74cfe3b570081
Bakuk <bakuk1996@mail.ru>
2022-09-03 23:18:20 +0700

    varuable change

--- functions/homeWork2/Program.cs
+++ functions/homeWork2/Program.cs
@@ -1,6 +1,6 @@
 ﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. */
 
-int a = 12;
+int a = Int32.Parse(Console.ReadLine());;
 
 void returnThirdNum(int n){
     string convertArray = n.ToString();
