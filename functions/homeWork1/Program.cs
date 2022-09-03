21b58afbcba64a28f81d48d535d04ed18db06023
Bakuk <bakuk1996@mail.ru>
2022-09-03 21:37:57 +0700

    functions homeWork1

---
+++ functions/homeWork1/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. */
+
+int returnSecondNumber(int n){
+    return n / 10 % 10;
+}
+
+
+int a = Int32.Parse(Console.ReadLine());;
+int res = returnSecondNumber(a);
+Console.WriteLine(res);
