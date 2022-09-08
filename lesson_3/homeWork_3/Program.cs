fa15a84c20be338f78fc5c619c49d5f9dbccf413
Bakuk <bakuk1996@mail.ru>
2022-09-08 21:51:11 +0700

    homeWork3

---
+++ lesson_3/homeWork_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+
+int prompt(string message){
+    System.Console.WriteLine(message);
+    int num = Int32.Parse(Console.ReadLine());
+    return num; 
+}
+
+void showCubNum(int n){
+    for(int i = 1; i<=n; i++){
+        System.Console.WriteLine(i * i * i);
+    }
+}
+
+int num = prompt("Введите число");
+showCubNum(num);
