b8742d3aa3e14ef88ec40d1abedf248e2c070cef
Bakuk <bakuk1996@mail.ru>
2022-09-10 20:27:37 +0700

    homeWork_1

---
+++ lesson_4/homeWork_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+int Prompt(string message){
+    System.Console.WriteLine(message);
+    int num = Int32.Parse(Console.ReadLine());
+    return num; 
+}
+
+int exp(int num1, int num2){
+    int res = 1;
+    for (int i = 1; i <= num2; i++){
+        res = res * num1;
+    }    
+    return res;
+}
+
+int a = Prompt("Введите число A");
+int b = Prompt("Введите число B");
+
+
+System.Console.WriteLine(exp(a,b));
