5a08e6be9a23f4502642d97a21eab22d15aace88
Bakuk <bakuk1996@mail.ru>
2022-09-10 21:28:27 +0700

    homeWork_3

---
+++ lesson_4/homeWork_3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+const int size = 8;
+int[] numbers = new int[size];
+
+fullArray(numbers);
+printArray(numbers);
+
+
+void fullArray(int[] array){
+    
+    for(int i = 0; i<array.Length; i++){
+        numbers[i] = new Random().Next(0, 10);
+    }
+}
+
+void printArray(int[] array){
+    for(int i = 0; i<array.Length; i++){
+        System.Console.Write($"{numbers[i]} ");
+    }
+}
+
+
