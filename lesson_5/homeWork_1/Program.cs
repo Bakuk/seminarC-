e4dc877687035b2a2b8f564ab9fa331a9d9c1af5
Bakuk <bakuk1996@mail.ru>
2022-09-15 22:35:28 +0700

    HomeWork1

---
+++ lesson_5/homeWork_1/Program.cs
@@ -0,0 +1,31 @@
+﻿const int size = 8;
+int[] numbers = new int[size];
+
+fullArray(numbers);
+printArray(numbers);
+System.Console.WriteLine();
+System.Console.WriteLine(countEven(numbers));
+
+
+void fullArray(int[] array){
+    
+    for(int i = 0; i<array.Length; i++){
+        numbers[i] = new Random().Next(0, 10);
+    }
+}
+
+void printArray(int[] array){
+    for(int i = 0; i<array.Length; i++){
+        System.Console.Write($"{numbers[i]} ");
+    }
+}
+
+int countEven(int[] array){
+    int count = 0;
+    for(int i = 0; i<array.Length; i++){
+        if(array[i] % 2 == 0){
+            count++;
+        }
+    }
+    return count;
+}
