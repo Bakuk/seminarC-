835075b65c85b43b984978a6bb354b9b118e9fdb
Bakuk <bakuk1996@mail.ru>
2022-09-17 22:07:04 +0700

    HomeWork_1

---
+++ lesson_6/homeWork_1/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = Prompt("Введите маскимальное число");
+int[] array = new int[n];
+fullArray(array);
+printArray(array);
+
+int Prompt(string message){
+    System.Console.WriteLine(message);
+    int num = Int32.Parse(Console.ReadLine());
+    return num; 
+}
+
+void fullArray(int[] array){
+    
+    for(int i = 0; i<array.Length; i++){
+        System.Console.Write("Введите число ");
+        array[i] = Int32.Parse(Console.ReadLine());
+    }
+}
+
+void printArray(int[] array){
+    int count = 0;
+    for(int i = 0; i<array.Length; i++){
+        if(array[i]>0) 
+            count++;
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine("Из них больше нуля " + count);
+}
+
+
