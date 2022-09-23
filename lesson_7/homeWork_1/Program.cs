b261039a72ffdfd35beff60c6c1cd5586b7304ae
Bakuk <bakuk1996@mail.ru>
2022-09-23 21:41:43 +0700

    lesson7 homeWork1

---
+++ lesson_7/homeWork_1/Program.cs
@@ -0,0 +1,32 @@
+﻿int Prompt(string message){
+    System.Console.WriteLine(message);
+    int num = Int32.Parse(Console.ReadLine());
+    return num; 
+}
+
+int n = Prompt("Введите n");
+int m = Prompt("Введите m");
+
+int[,] array = new int[n,m];
+
+FillArray(array);
+PrintArray(array);
+
+
+void PrintArray(int[,] arr){
+    for (int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            System.Console.Write(" " + array[i,j]); 
+        }
+    System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] arr){
+    for (int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            array[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
