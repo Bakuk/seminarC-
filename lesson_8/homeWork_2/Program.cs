a5119c656f5eb9fd3e7d606ad7fcf07caaa6f43e
Bakuk <bakuk1996@mail.ru>
2022-09-28 00:58:08 +0700

    lesson_8

---
+++ lesson_8/homeWork_2/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 2: Задайте две квадратные матрицы одного размера. Напишите программу, которая будет находить произведение двух матриц. */
+
+//int row = Prompt("Введите строку");
+//int col = Prompt("Введите столбец");
+
+int[,] a  = 
+            {{2, 4},
+             {3, 2}};
+
+int[,] b = {
+            {3,4},
+            {3,3}};
+
+int[,] c = new int[2,2];
+c = MultMatrix(a, b);
+//FillArray(array);
+PrintArray(a);
+System.Console.WriteLine();
+PrintArray(b);
+PrintArray(c);
+
+int[,] MultMatrix(int[,] m1, int[,] m2){
+    int[,] r = new int[2,2];
+    for (int i = 0; i < m1.GetLength(0); i++)
+            {
+                for (int j = 0; j < m2.GetLength(1); j++)
+                {
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                    {
+                        r[i,j] += m1[i,k] * m2[k,j];
+                    }
+                }
+            } 
+    return r;
+}
+
+/* void FillArray(int[,] arr){
+    for (int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            array[i,j] = new Random().Next(0, 10);
+        }
+    }
+} */
+
+/* int Prompt(string message){
+    System.Console.WriteLine(message);
+    int num = Int32.Parse(Console.ReadLine());
+    return num; 
+} */
+
+void PrintArray(int[,] arr){
+    for (int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            System.Console.Write(" " + arr[i,j]); 
+        }
+    System.Console.WriteLine();
+    }
+}
+
---
+++ lesson_8/homeWork_3/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 3. Сформируйте двухмерный массив из неповторяющихся случайных двузначных чисел (размер массива не более 50 элементов). Напишите программу, которая будет построчно выводить массив. */
+
+int row = Prompt("Введите строку");
+int col = Prompt("Введите столбец");
+
+int[,] array = new int[col,row];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+bool check = Contains(array, 24);
+if(check){
+    System.Console.WriteLine("Присутствует");
+} else{
+    System.Console.WriteLine("Не присутствует");
+}
+
+
+void FillArray(int[,] arr){
+    for (int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            array[i,j] = new Random().Next(10, 99);
+        }
+    }
+}
+
+int Prompt(string message){
+    System.Console.WriteLine(message);
+    int num = Int32.Parse(Console.ReadLine());
+    return num; 
+}
+
+void PrintArray(int[,] arr){
+    for (int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            System.Console.Write(" " + array[i,j]); 
+        }
+    System.Console.WriteLine();
+    }
+}
+
+bool Contains(int[,] arr, int value){
+    bool check = false;
+    for (int i = 0; i<arr.GetLength(0); i++){
+        for(int j = 0; j<arr.GetLength(1); j++){
+            if(arr[i,j] == value){
+                check = true;
+                break;
+            }
+        }
+        if(check){
+            break;
+        } 
+    }
+    return check;
+}
