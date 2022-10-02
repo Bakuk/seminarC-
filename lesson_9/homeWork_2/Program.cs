16e69129a9f60d6b0b33bd96bf0889f3840305d4
Bakuk <bakuk1996@mail.ru>
2022-10-02 17:45:55 +0700

    lesson 9 homeWork2

---
+++ lesson_9/homeWork_2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int m = 1;
+int n = 15;
+System.Console.WriteLine(SumRec(m,n));
+
+int SumRec(int m, int n){
+    if(m == n)
+        return m;
+    return SumRec(m+1, n) + m;
+}
